843ab0f91cdcab12535c850e7179f481de7e55bb
euanb <euan.birk@gmail.com>
2025-09-23 17:48:10 +0100

    Adding mongodb docker connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get collection from database
